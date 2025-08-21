1d74f71ae3814cb482419023bd148408fd82b766
MarcusYim <marcusyim2004@gmail.com>
2025-08-21 14:42:54 -0700

    improvements

--- src/geometry/matrix/distance.py
+++ src/geometry/matrix/distance.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any, Iterable, Iterator, Optional, Union
 
@@ -13,7 +14,7 @@ from ...object.metadata import DistanceType
 from ...object.points import Points
 
 
-class DistanceMatrix(BaseArray, GeometryMatrixMixin):
+class DistanceMatrix(BaseArray, GeometryMatrixMixin, ABC):
     def __new__(cls, *args, **kwargs):
         if cls is DistanceMatrix:
             if 'shape' in kwargs:
@@ -25,7 +26,7 @@ class DistanceMatrix(BaseArray, GeometryMatrixMixin):
         self,
         radius: float,
         in_place: bool = False,
-    ) -> "DistanceMatrix":
+    ) -> DistanceMatrix:
         """
         :param dist_mat:
         :param radius:
@@ -42,43 +43,41 @@ class DistanceMatrix(BaseArray, GeometryMatrixMixin):
         return self._execute_threshold(radius, in_place)
 
     @abstractmethod
-    def _execute_threshold(self, radius: float, in_place: bool = False) -> "DistanceMatrix":
+    def _execute_threshold(self, radius: float, in_place: bool = False) -> DistanceMatrix:
         pass
+    
+    def threshold_distance_iter(
+        self,
+        radii: Union[float, Iterable[float]],
+        in_place: bool = False,
+    ) -> Iterator[DistanceMatrix]:
+        """
+        Take a distance matrix and eliminate entries at the new radii. Note that we return an iterator
+        that could be used by other functions down stream without storing all matrices.
+        :param dist_mat:
+        :param radii:
+        :param in_place:
+        :return:
+        """
+
+        if isinstance(radii, float):
+            radii = (radii,)
+
+        for radius in reversed(sorted(radii)):
+            yield self.threshold(radius, in_place)
 
 
 class DenseDistanceMatrix(DenseArray, DistanceMatrix):
     def _execute_threshold(
         self, radius: float, in_place: bool
-    ) -> "DistanceMatrix":
+    ) -> DistanceMatrix:
         dist_mat = self if in_place else self.copy()
         dist_mat[dist_mat > radius] = np.inf
         return dist_mat
 
+
 class SparseDistanceMatrix(SparseArray, DistanceMatrix):
     def _execute_threshold(
         self, radius: float, in_place: bool
-    ) -> "DistanceMatrix":
-        pass
-
-
-def threshold_distance_iter(
-    dist_mat: DistanceMatrix,
-    radii: Union[float, Iterable[float]],
-    in_place: bool = False,
-) -> Iterator[DistanceMatrix]:
-    """
-    Take a distance matrix and eliminate entries at the new radii. Note that we return an iterator
-    that could be used by other functions down stream without storing all matrices.
-    :param dist_mat:
-    :param radii:
-    :param in_place:
-    :return:
-    """
-
-    if isinstance(radii, float):
-        radii = (radii,)
-
-    for radius in reversed(sorted(radii)):
-        print(radius)
-        print(threshold_distance(dist_mat, radius, in_place).data.storage)
-        yield threshold_distance(dist_mat, radius, in_place)
+    ) -> DistanceMatrix:
+        raise NotImplementedError()
