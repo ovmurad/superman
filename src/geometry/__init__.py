e4646dccc36f98b6eda257d43bd31dc48aedcd23
MarcusYim <marcusyim2004@gmail.com>
2025-08-25 16:24:35 -0700

    fixes

--- src/geometry/__init__.py
+++ src/geometry/__init__.py
@@ -1,5 +1,6 @@
 from .matrix import AdjacencyMatrix, AffinityMatrix, DistanceMatrix, LaplacianMatrix
 from .points import Coordinates, Data, Embedding, Points
+from .normalize import normalize
 
 __all__ = [
     "AdjacencyMatrix",
@@ -10,4 +11,5 @@ __all__ = [
     "Embedding",
     "Data",
     "Coordinates",
+    "normalize",
 ]
